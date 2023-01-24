eeb7df07d831497954ace2b711adb01d0f955195
Kennie <Kenneth Chan>
2023-01-23 21:16:39 -0500

    complete exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,16 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+store = Store.create(name: "Surrey", annual_revenue:224000, mens_apparel: false, womens_apparel: true)
+store = Store.create(name: "Whistler", annual_revenue:1900000, mens_apparel: true, womens_apparel: false)
+store = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each {|store| puts "Name: #{store.name} and Annual Revenue: #{store.annual_revenue}"}
+
+# specifically ... to exclude 1mil
+@womens_under_mil = Store.where(womens_apparel: true, annual_revenue: 1...1000000)
+
+@womens_under_mil.each {|store| puts "Name: #{store.name} and Annual Revenue: #{store.annual_revenue}"}
